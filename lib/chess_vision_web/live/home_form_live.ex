48a2d4857ad064070d831111f71fccc2903d7e8b
Jimbonk Buchanon <jimbonkgit@gmail.com>
2023-07-24 08:21:31 -0400

    Get canny edge detection working with imageproc

--- lib/chess_vision_web/live/home_form_live.ex
+++ lib/chess_vision_web/live/home_form_live.ex
@@ -5,7 +5,7 @@ defmodule ChessVisionWeb.HomeFormLive do
   def mount(_params, _session, socket) do
     {:ok,
      socket
-     |> assign(uploaded_image: nil, fen: nil)
+     |> assign(uploaded_image: nil)
      |> allow_upload(:board_image, accept: ~w(image/*), max_entries: 1)}
   end
 
@@ -17,9 +17,8 @@ defmodule ChessVisionWeb.HomeFormLive do
   @impl Phoenix.LiveView
   def handle_event("capture", _params, socket) do
     image_path = save_file(socket)
-    res = ChessVision.ImageRecognition.canny_edge_detection(image_path)
-    IO.inspect(res, label: "Canny Edge result")
-    {:noreply, assign(socket, uploaded_image: image_path, fen: res)}
+    ChessVision.ImageRecognition.canny_edge_detection(image_path)
+    {:noreply, assign(socket, uploaded_image: image_path)}
   end
 
   defp save_file(socket) do
--- native/chess_vision/src/lib.rs
+++ native/chess_vision/src/lib.rs
@@ -1,5 +1,3 @@
-use image::{GrayImage, ImageBuffer, RgbImage};
-
 #[rustler::nif]
 fn canny_edge_detection(image_path: String) {
     let priv_image_path = format!("priv/static{}", image_path);
@@ -9,36 +7,22 @@ fn canny_edge_detection(image_path: String) {
     let reader = image::io::Reader::new(std::io::Cursor::new(bytes))
         .with_guessed_format()
         .expect("Cursor failed");
-    let image = reader.decode().expect("Decoding failed");
+    let grayscale_image = reader.decode().expect("Decoding failed").into_luma8();
+
+    // Get median pixel value
+    // for pixel in grayscale_image.pixels {
+    //   let val = pixel.0[0];
+    // }
 
     // Perform Canny edge detection with specified parameters
-    let sigma = 1.0;
-    let high_threshold = 100;
-    let low_threshold = 50;
-    let edges = _canny_edge_detection(&image, sigma, high_threshold, low_threshold);
+    let high_threshold = 100.0;
+    let low_threshold = 50.0;
+    let edges = imageproc::edges::canny(&grayscale_image, low_threshold, high_threshold);
 
     // Save the resulting edges image
     edges
-        .save("../../priv/static/edges.jpg")
+        .save("image_output/edges.jpg")
         .expect("Failed to save image");
-}
-
-fn _canny_edge_detection(
-    image: &RgbImage,
-    sigma: f32,
-    high_threshold: u8,
-    low_threshold: u8,
-) -> RgbImage {
-    let grayscale = image.into_luma();
-    // let blurred = gaussian_blur::<f32>(&grayscale, sigma);
-    // let (gradient_magnitude, gradient_direction) = calculate_gradients(&blurred);
-    // let suppressed = non_maximum_suppression(&gradient_magnitude, &gradient_direction);
-    // let edges = double_thresholding(&suppressed, high_threshold, low_threshold);
-
-    ImageBuffer::from_fn(image.width(), image.height(), |x, y| {
-        let edge_pixel = edges[(y as u32, x as u32)];
-        image::Rgb([edge_pixel, edge_pixel, edge_pixel])
-    })
 }
 
 rustler::init!(
