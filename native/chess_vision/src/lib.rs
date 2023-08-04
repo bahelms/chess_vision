394f3b84b267d9275ace7e4b9ad10aec26881f9f
Jimbonk Buchanon <jimbonkgit@gmail.com>
2023-08-04 15:14:42 -0400

    Finishes hough line transform

--- native/chess_vision/src/lib.rs
+++ native/chess_vision/src/lib.rs
@@ -1,31 +1,63 @@
+use opencv::prelude::*;
+use opencv::{core, imgcodecs, imgproc};
+use std::f64::consts::PI;
+
 #[rustler::nif]
-fn canny_edge_detection(image_path: String) {
-    let priv_image_path = format!("priv/static{}", image_path);
+fn detect_chessboard(image_file: String) {
+    // grayscale
+    let image = imgcodecs::imread(&image_file, imgcodecs::IMREAD_GRAYSCALE)
+        .expect("Image file could not be read");
+    save_image("grayscale.jpg", &image);
 
-    // Load an image using the `image` crate
-    let bytes = std::fs::read(priv_image_path).unwrap();
-    let reader = image::io::Reader::new(std::io::Cursor::new(bytes))
-        .with_guessed_format()
-        .expect("Cursor failed");
-    let grayscale_image = reader.decode().expect("Decoding failed").into_luma8();
+    let max_width = image.rows();
 
-    // Get median pixel value
-    // for pixel in grayscale_image.pixels {
-    //   let val = pixel.0[0];
-    // }
+    // canny edge detection
+    let mut edges = image.clone();
+    imgproc::canny(&image, &mut edges, 150.0, 190.0, 3, false).expect("Failed to execute canny");
+    save_image("edges.jpg", &edges);
 
-    // Perform Canny edge detection with specified parameters
-    let high_threshold = 100.0;
-    let low_threshold = 50.0;
-    let edges = imageproc::edges::canny(&grayscale_image, low_threshold, high_threshold);
-
-    // Save the resulting edges image
-    edges
-        .save("image_output/edges.jpg")
-        .expect("Failed to save image");
+    // hough line transform
+    let mut lines = edges.clone();
+    imgproc::hough_lines(&edges, &mut lines, 1.0, PI / 180.0, 1100, 0.0, 0.0, 0.0, PI)
+        .expect("Failed to execute hough_lines");
+    save_lines_image(lines, &edges, max_width as f32)
 }
 
-rustler::init!(
-    "Elixir.ChessVision.ImageRecognition",
-    [canny_edge_detection]
-);
+fn save_lines_image(lines: core::Mat, edges: &core::Mat, max_width: f32) {
+    let mut lines_image = edges.clone();
+    imgproc::cvt_color(&edges, &mut lines_image, imgproc::COLOR_GRAY2BGR, 0)
+        .expect("Failed to convert edges color");
+
+    for row in 0..lines.rows() {
+        for col in 0..lines.cols() {
+            let line = lines.at_2d::<core::Vec2f>(row, col).unwrap();
+            let (pt1, pt2) = convert_to_polar_lines(line, max_width);
+            let red = core::Scalar::new(0.0, 0.0, 255.0, 0.0);
+            imgproc::line(&mut lines_image, pt1, pt2, red, 3, imgproc::LINE_AA, 0)
+                .expect("Failed to draw line");
+        }
+    }
+    save_image("lines.jpg", &lines_image);
+}
+
+// converts Hough space (rho, theta) to polar space (x, y)
+fn convert_to_polar_lines(line: &core::Vec2f, max_width: f32) -> (core::Point, core::Point) {
+    let rho = line[0];
+    let theta = line[1];
+    let a = theta.cos();
+    let b = theta.sin();
+    let x = a * rho;
+    let y = b * rho;
+    let pt1 = core::Point::new((x + max_width * -b) as i32, (y + max_width * a) as i32);
+    let pt2 = core::Point::new((x - max_width * -b) as i32, (y - max_width * a) as i32);
+    (pt1, pt2)
+}
+
+fn save_image(filename: &str, image: &Mat) {
+    let write_params = core::Vector::new();
+    let filepath = format!("image_output/{}", filename);
+    let failure = format!("Failed to write {}", filename);
+    imgcodecs::imwrite(&filepath, &image, &write_params).expect(&failure);
+}
+
+rustler::init!("Elixir.ChessVision.ImageRecognition", [detect_chessboard]);
